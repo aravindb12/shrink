583d4b0ef39c3ea258a574016ed4036b801c337b
Aravind <aravindsbaskaran@gmail.com>
2021-04-18 22:40:32 +0530

    configure app component to typescript

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
-import '../styles/globals.css'
+// import App from "next/app";
+import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
